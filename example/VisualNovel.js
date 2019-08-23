2fd0b6c72eff0668cc19464fae3b366d47060e29
Bernard Lee <38368050+tejaboy@users.noreply.github.com>
2019-08-23 17:43:22 +0800

    switch to tab-based indendation.

--- example/VisualNovel.js
+++ example/VisualNovel.js
@@ -3,22 +3,22 @@ startGame();
 
 async function startGame()
 {
-    preload();
-    
-    WeiSen.set_size("100vh", "100vw");
-    WeiSen.set_background("background.jpg");
-    
-    JingSen = new Character("Jing Sen");
-    
-    JingSen.show("JingSenConfident", {"right": 0, "bottom": 0});
-    await JingSen.say("Hey there!");
-    var reply = await JingSen.ask_choose(["None of your business", "The best guy ever", "The most handsome guy"], "Who are you?");
+	preload();
+	
+	WeiSen.set_size("100vh", "100vw");
+	WeiSen.set_background("background.jpg");
+	
+	JingSen = new Character("Jing Sen");
+	
+	JingSen.show("JingSenConfident", {"right": 0, "bottom": 0});
+	await JingSen.say("Hey there!");
+	var reply = await JingSen.ask_choose(["None of your business", "The best guy ever", "The most handsome guy"], "Who are you?");
    	await JingSen.say(reply + "?!?!");
-    await JingSen.say("Stop fooling around! Tell me your name!");
-    var MC = new Character(await WeiSen.prompt("Enter your name: ", "Bernard"));
+	await JingSen.say("Stop fooling around! Tell me your name!");
+	var MC = new Character(await WeiSen.prompt("Enter your name: ", "Bernard"));
 }
 
 function preload()
 {
-    WeiSen.preload_image('JingSenConfident', 'Confident.png');
+	WeiSen.preload_image('JingSenConfident', 'Confident.png');
 }
